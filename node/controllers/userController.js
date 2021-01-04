331605d632a7e50e314795ef61984a12088ece8b
jota <jato24ty2@protonmail.com>
2021-01-04 14:51:56 +0100

    quitar login

--- node/controllers/userController.js
+++ node/controllers/userController.js
@@ -2,121 +2,14 @@ require("dotenv").config();
 const userRepository = require("../repository/user.js");
 const bcrypt = require("bcryptjs");
 const Joi = require("joi");
-const { MAILGUN_KEY, DOMAIN } = process.env;
-const mailgun = require("mailgun-js");
-const jwt = require("jsonwebtoken");
+// const { MAILGUN_KEY, DOMAIN } = process.env;
+// const mailgun = require("mailgun-js");
+// const jwt = require("jsonwebtoken");
 
 async function getUsers(req, res) {
   try {
     const user = await userRepository.getUser();
     res.send(user);
-  } catch (err) {
-    if (err.name === "ValdationError") {
-      err.code = 400;
-    }
-    console.log(err);
-    res.status(err.status || 500);
-    res.send({ error: err.message });
-  }
-}
-
-async function register(req, res) {
-  try {
-    const schema = Joi.object({
-      username: Joi.string().required(),
-      email: Joi.string().email().required(),
-      userPassword: Joi.string().min(8).required(),
-      repeatPassword: Joi.ref("contraseña"),
-    });
-    await schema.validateAsync(req.body);
-    const { username, email, userPassword } = req.body;
-
-    const passwordHash = await bcrypt.hash(userPassword, 10);
-    const newUser = await userRepository.createUser(
-      username,
-      email,
-      passwordHash
-    );
-    const mg = mailgun({
-      apiKey: MAILGUN_KEY,
-      domain: DOMAIN,
-    });
-
-    const data = {
-      from: "sender@gmail.com",
-      to: "jeimymiranda@outlook.es",
-      subject: "Hola",
-      text: "Testing some Mailgun awesomness!",
-    };
-    await mg.messages().send(data);
-
-    res.send(newUser);
-  } catch (err) {
-    if (err.name === "ValdationError") {
-      err.code = 400;
-    }
-    console.log(err);
-    res.status(err.status || 500);
-    res.send({ error: err.message });
-  }
-}
-
-async function login(req, res) {
-  try {
-    const schema = Joi.object({
-      email: Joi.string().email().required(),
-      userPassword: Joi.string().min(8).required(),
-    });
-    await schema.validateAsync(req.body);
-    const { email, userPassword } = req.body;
-    const user = await userRepository.logearse(email);
-    if (!user) {
-      console.log("error");
-    }
-    const validPassword = await bcrypt.compare(
-      userPassword,
-      user[0].userPassword
-    );
-    if (!validPassword) {
-      console.log("error");
-    }
-
-    const tokePayload = { id: user[0].id, name: user[0].username };
-    const token = jwt.sign(tokePayload, process.env.JWT, { expiresIn: "30d" });
-    res.send(token);
-  } catch (err) {
-    if (err.name === "ValdationError") {
-      err.code = 400;
-    }
-    console.log(err);
-    res.status(err.status || 500);
-    res.send({ error: err.message });
-  }
-}
-
-async function getUserSelect(req, res) {
-  try {
-    const userId = req.params.userId;
-    const selectUser = await userRepository.selectUser(userId);
-
-    res.send(selectUser);
-  } catch (err) {
-    if (err.name === "ValdationError") {
-      err.code = 400;
-    }
-    console.log(err);
-    res.status(err.status || 500);
-    res.send({ error: err.message });
-  }
-}
-
-async function upImgUser(req, res) {
-  try {
-    const userId = req.params.userId;
-    const imgUser = req.file.path;
-
-    const img = await userRepository.imageUser(imgUser, userId);
-    res.send(img);
   } catch (err) {
     if (err.name === "ValdationError") {
       err.code = 400;
@@ -150,9 +43,6 @@ async function newPassword(req, res) {
 
 module.exports = {
   getUsers,
-  register,
-  login,
-  getUserSelect,
-  upImgUser,
+
   newPassword,
 };
--- node/repository/user.js
+++ node/repository/user.js
@@ -7,36 +7,6 @@ async function getUser() {
   return users;
 }
 
-async function createUser(name, email, password) {
-  const pool = await database.getPool();
-  const insertQuery =
-    "insert into user (username, email, userPassword) values(?,?,?) ";
-  const [createUser] = await pool.query(insertQuery, [name, email, password]);
-  return createUser.isertId;
-}
-
-async function logearse(email) {
-  const pool = await database.getPool();
-  const query = "select * from user where email= ?";
-  const [user] = await pool.query(query, email);
-  return user;
-}
-
-async function selectUser(userId) {
-  const pool = await database.getPool();
-  const query = "select * from user where id_user = ?";
-  const [user] = await pool.query(query, userId);
-  return user;
-}
-
-async function imageUser(img, userId) {
-  const pool = await database.getPool();
-
-  const query = "update user set photo = ? where id_user = ?";
-  const [image] = await pool.query(query, [img, userId]);
-  return image;
-}
-
 async function changePassword(password, userId) {
   const pool = await database.getPool();
 
@@ -47,9 +17,6 @@ async function changePassword(password, userId) {
 
 module.exports = {
   getUser,
-  createUser,
-  logearse,
-  selectUser,
-  imageUser,
+
   changePassword,
 };
