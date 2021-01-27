0df8de16f6f8603c1d6f9d53a7d1cb564cee927a
jota <jato24ty2@protonmail.com>
2021-01-27 11:02:18 +0100

    principalPage

--- app/src/components/principalPage.js
+++ app/src/components/principalPage.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Principal = () => {
+// const useFetchData = (url) => {
+//   const [data, setData] = useState([]);
+//   useEffect(() => {
+//     fetch(url)
+//       .then((res) => res.json())
+//       .then((res) => setData(res, (msg) => console.log("Err:", msg)));
+//   }, []);
+//   return [data, setData];
+// }
+
+
+
+const Principal = async () => {
+  const [data, setData] = useState('')
+  const res = await fetch("http://cors-bridge.herokuapp.com/http://localhost:8084/api/user")
+      .then((res) => res.json())
+      .then((res) => setData(res, (msg) => console.log("Err:", msg)));
+ 
+
   return (
     <div>
       <h1>Recy-book</h1>
-      <p>Pepe</p>
+      <p>{res}</p>
     </div>
   );
 };
